cb9fde73e1dcf75a6b39c8c1cd0faa7138d690e4
Igor Smirnov <smirnoviv@rambler.ru>
2017-03-02 14:44:29 +0300

    Developing image writiing

--- stereo/stereo.cpp
+++ stereo/stereo.cpp
@@ -6,14 +6,19 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/calib3d.hpp>
+#include <Windows.h>
+#include <ctime>
 
 int main()
 {
 	Rectifier rct("log2s.txt");
 	cv::VideoCapture vcl(0), vcr(1);
 	int nod = 128;
+	int idx = 0;
+	std::string wdir = "";
 	auto sm = cv::StereoBM::create(nod, 21);
 	//auto sm = cv::StereoSGBM::create(5, nod, 15);
+	int k;
 	do {
 		cv::Mat left, right, left_g, right_g, left_r, right_r, disp, disp8;
 		vcl >> left;
@@ -27,6 +32,12 @@ int main()
 		rct.rectify(right_g, right_r, 1);
 		//cv::equalizeHist(right_r, right_r);
 
+		if (wdir != "") {
+			char buf[32];
+			sprintf_s(buf, "%04d", idx++);
+			//cv::imwrite()
+
+		}
 		cv::imshow("Left", left_r);
 		cv::imshow("Right", right_r);
 
@@ -34,7 +45,27 @@ int main()
 		disp.convertTo(disp8, CV_8U, 255 / (nod * 16.));
 		//cv::
 		cv::imshow("Disparity", disp8);
-	} while (cv::waitKey(1) != 27);
+		k = cv::waitKey(1);
+		switch (k) {
+		case 'w': if (wdir == "") {
+			idx = 0;
+			time_t rawtime;
+			struct tm timeinfo;
+			char buffer[128];
+
+			time(&rawtime);
+			localtime_s(&timeinfo, &rawtime);
+
+			strftime(buffer, 128, "d:/wires/%Y-%m-%d %I:%M:%S/", &timeinfo);
+			if (CreateDirectoryA(buffer, NULL) || ERROR_ALREADY_EXISTS == GetLastError())
+				wdir = buffer;
+			break;
+		}
+		case 's': 
+			wdir = "";
+			break;
+		}
+	} while (k != 27);
 
     return 0;
 }
